90e3b25fd942e81e2c228f614637830c1b9b125d
przemyslawren <przemyslaw.ren@gmail.com>
2025-07-02 14:23:28 +0200

    Init project

---
+++ src/main/java/org/akka/Main.java
@@ -0,0 +1,18 @@
+package org.akka;
+
+import akka.actor.typed.ActorSystem;
+
+public class Main {
+//    public static void main(String[] args) {
+//        ActorSystem<String> actorSystem = ActorSystem.create(FirstSimpleBehavior.create(), "FirstActorSystem");
+//        actorSystem.tell("say hello");
+//        actorSystem.tell("who are you");
+//        actorSystem.tell("create a child");
+//        actorSystem.tell("Random message");
+//    }
+
+    public static void main(String[] args) {
+        ActorSystem<String> actorSystem = ActorSystem.create(ManagerBehavior.create(), "ProbablePrimeSystem");
+        actorSystem.tell("start");
+    }
+}
